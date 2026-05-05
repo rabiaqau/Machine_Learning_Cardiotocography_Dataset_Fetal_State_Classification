a540d9b7b3bbbd86de88743f019f2d39459ab880
Rabia Shaheen <rshaheen.kth@gmail.com>
2026-05-05 18:40:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
+
 import streamlit as st
 import joblib
+import pandas as pd
 
-st.title("Test App")
+model = joblib.load("/Users/rabiashaheen/Desktop/ml_project/Machine_Learning_Cardiotocography_Dataset_Fetal_State_Classification/rf_best_model.pkl")
 
-st.write("Starting...")
+feature_names = [
+    'LB','AC','FM','UC','DL','DS','DP',
+    'ASTV','MSTV','ALTV','MLTV',
+    'Width','Min','Max','Nmax','Nzeros',
+    'Mode','Mean','Median','Variance','Tendency'
+]
 
-try:
-    model = joblib.load("rf_best_model.pkl")
-    st.success("RF model loaded")
-except Exception as e:
-    st.error("Error loading RF model")
-    st.code(str(e))
+st.title("Cardiotocography Prediction")
+
+input_data = {}
+for feature in feature_names:
+    input_data[feature] = st.number_input(feature, value=0.0)
+
+input_df = pd.DataFrame([input_data])[feature_names]
+
+if st.button("Predict"):
+    pred = model.predict(input_df)
+    st.success(f"Prediction: {pred[0]}")
